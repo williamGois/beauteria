2d4248066d67a66061c81c56ada8d58f9ca7e405
william gois <goiswilliam194@gmail.com>
2024-01-30 20:18:07 -0300

    Ajuste de titulo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +21,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
